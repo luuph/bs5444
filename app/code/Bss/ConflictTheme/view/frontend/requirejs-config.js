f1d1254e382c2c15afcb706b6d02498aa6faa28d
Hai Luu <luuph@bsscommerce.com>
2021-09-16 18:03:23 +0700

    Fix conflict theme

---
+++ app/code/Bss/ConflictTheme/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Rokanthemes_AjaxSuite/js/ajaxsuite': {
+                'Bss_ConflictTheme/js/ajaxsuite-mixin': true
+            }
+        }
+    }
+};
